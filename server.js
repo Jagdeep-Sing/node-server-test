881b80296e612318c25a08727a975c9accfbf734
J4gd33p 51ngh <js86248@gmail.com>
2018-05-28 23:40:33 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Homepage',
+    welcomeMsg: 'Welcome to the Awesomeness of your DREAMS'
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('App Listening on port 3000');
+});
